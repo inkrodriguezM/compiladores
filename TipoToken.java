b05ad34aa41abce07a4fa3cd0e891a0a3c3f4042
inkrodriguezM <irodriguezm1702@alumno.ipn.mx>
2023-03-24 01:01:06 -0600

    Agrego codigo base
    Codigo base para el analizador lexico

---
+++ TipoToken.java
@@ -0,0 +1,14 @@
+package mx.ipn.escom.compiladores;
+
+public enum TipoToken {
+    // Crear un tipoToken por palabra reservada
+    // Crear un tipoToken: identificador, una cadena y numero
+    // Crear un tipoToken por cada "Signo del lenguaje" (ver clase Scanner)
+
+
+    // Palabras clave:
+    Y, CLASE,
+
+    // Final de cadena
+    EOF
+}
